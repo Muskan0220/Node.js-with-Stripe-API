477a5e2f8d947ae043cc6b676bc215234f5422c7
MUSKAN AGRAWAL <63292583+Muskan0220@users.noreply.github.com>
2020-11-26 16:45:57 +0530

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stripePublishableKey:'pk_live_51HrPjPGs5m7je9z9kfFpbqeiSKtkhqApwYIhandJu3tlwGeV2Fsvb1wc8cscqouU7L71y4kAwKbDcxzlYLjqN88t00BVn0x7fc',
-  stripeSecretKey:'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'
-}
